b7014a2b2f6eb66caced54e48c68077d4361748b
LEMAOANA <lejoneedwardl@gmail.com>
2025-10-29 14:34:06 +0200

    final one

--- frontend/src/components/feedbacklist.js
+++ frontend/src/components/feedbacklist.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const FeedbackList = () => {
   const [feedbacks, setFeedbacks] = useState([]);
